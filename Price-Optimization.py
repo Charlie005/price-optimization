8b5581556a2fe520de919e6000a24ff25b0ac0d7
Amal Joseph John <36783116+Charlie005@users.noreply.github.com>
2022-06-02 09:36:35 +0530

    Update Price-Optimization.py

--- Price-Optimization.py
+++ Price-Optimization.py
@@ -11,8 +11,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
 
-df = pd.read_csv("https://raw.githubusercontent.com/Charlie005/price-optimization/master/priceOptimize.csv")
-#df3  = pd.read_csv("https://raw.githubusercontent.com/Charlie005/price-optimization/master/coefs.csv")
+#df = pd.read_csv("https://raw.githubusercontent.com/Charlie005/price-optimization/master/priceOptimize.csv")
+df  = pd.read_csv("https://raw.githubusercontent.com/Charlie005/price-optimization/master/coefs.csv")
 
 names = ['< PRODUCT >']
 categories = ['< CATEGORY >'] + sorted(df['MC'].unique().tolist())
@@ -20,45 +20,56 @@ brands = ['< BRAND >']
 # cName = df['NAME'].value_counts()
 zones = ['< ZONE >']
 
-df.dropna(inplace=True)
+# # For creating CSV file with coefs
+# def load_data():   
+#     global df
+#     global df3
+#     global categories, brands, names
+#     df = pd.read_csv("C:/Data/priceOptimize.csv")
+#     df.dropna(inplace=True)
 
-df = df.loc[df['Sales_at_Cost'] >= 0]
-df = df.loc[df['NSU'] >= 0]
-df = df.loc[df['MRP'] >= 0]
-df = df.loc[df['SP'] >= 0]
+#     df = df.loc[df['Sales_at_Cost'] >= 0]
+#     df = df.loc[df['NSU'] >= 0]
+#     df = df.loc[df['MRP'] >= 0]
+#     df = df.loc[df['SP'] >= 0]
 
 
-df['UC'] = df['Sales_at_Cost']/df['NSU']
-df['UGST'] = df['GST']/df['NSU']
-names = ['< PRODUCT >'] + sorted(df['NAME'].unique().tolist())
-categories = ['< CATEGORY >'] + sorted(df['MC'].unique().tolist())
-brands = ['< BRAND >'] + sorted(df['Brand'].unique().tolist())
-#cName = df['NAME'].value_counts()
-zones = ['< ZONE >','NORTH','SOUTH','EAST','WEST']
-dfdict = {}
-for i in names:
-    df1 = df.loc[df['NAME'] == i]
-    dfdict[i] = df1
-  
-df3 = pd.DataFrame(columns=['NAME','ZONE','Intercept','SP_Coef','UC_Coef','UC','UGST'])    
-for k in dfdict:
-    zgroups = dfdict[k].groupby('ZONE')
-    zonevals = zgroups.groups.keys()
-    for i in zonevals:
-        zval = zgroups.get_group(i)
-        model = ols("NSU ~ SP + UC", data = zval).fit()
-        p = model.params
-        cost = dfdict[k].UC.mean()
-        gst = dfdict[k].UGST.mean()
-        temp = {'NAME':k,'ZONE':i, 'Intercept':p[0], 'SP_Coef':p[1], 'UC_Coef':p[2], 'UC':cost, 'UGST':gst}
-        df3 = df3.append(temp,ignore_index=True)
-#df3.to_csv('coefs.csv')
+#     df['UC'] = df['Sales_at_Cost']/df['NSU']
+#     df['UGST'] = df['GST']/df['NSU']
+#     names = ['< PRODUCT >'] + sorted(df['NAME'].unique().tolist())
+#     categories = ['< CATEGORY >'] + sorted(df['MC'].unique().tolist())
+#     brands = ['< BRAND >'] + sorted(df['Brand'].unique().tolist())
+#     #cName = df['NAME'].value_counts()
+#     zones = ['< ZONE >','NORTH','SOUTH','EAST','WEST']
+#     dfdict = {}
+#     for i in names:
+#         df1 = df.loc[df['NAME'] == i]
+#         dfdict[i] = df1
+      
+#     df3 = pd.DataFrame(columns=['NAME','Brand','MC','ZONE','Intercept','SP_Coef','UC_Coef','UC','UGST'])    
+#     for k in dfdict:
+#         zgroups = dfdict[k].groupby('ZONE')
+#         zones = zgroups.groups.keys()
+#         for i in zones:
+#             zval = zgroups.get_group(i)
+#             model = ols("NSU ~ SP + UC", data = zval).fit()
+#             p = model.params
+#             cost = dfdict[k].UC.mean()
+#             gst = dfdict[k].UGST.mean()
+#             mc = dfdict[k].MC.iloc[0]
+#             brnd = dfdict[k].Brand.iloc[0]
+#             minmrp = min(dfdict[k].MRP)
+#             maxmrp = max(dfdict[k].MRP)
+#             pricerange = str(minmrp) + ' - ' + str(maxmrp)
+#             temp = {'NAME':k,'Brand':brnd,'MC':mc,'ZONE':i, 'Intercept':p[0], 'SP_Coef':p[1], 'UC_Coef':p[2], 'UC':cost, 'UGST':gst,'Price_Range':pricerange}
+#             df3 = df3.append(temp,ignore_index=True)
+#     df3.to_csv('coefs.csv')
 
+#load_data()
 
 def getprice():
     
-                val = df3.loc[(df3['NAME'] == product) & (df3['ZONE'] == zone)]
-            
+                val = df.loc[(df['NAME'] == product) & (df['ZONE'] == zone)]
                 S = []
                 for i in np.arange(mrp/2,mrp,0.01):
                      S = S + [i]
@@ -131,15 +142,14 @@ if(brand != '< BRAND >'):
     names = ['< PRODUCT >'] + sorted(val['NAME'].unique().tolist())
 product = col3.selectbox('Select Product',names,index = names.index('< PRODUCT >'))
 if(product != '< PRODUCT >'):
-    val = df3.loc[(df3['NAME'] == product)]
+    val = df.loc[(df['NAME'] == product)]
     zones = ['< ZONE >'] + val['ZONE'].unique().tolist()
 
 zone = col4.selectbox('Select Zone',zones,index = zones.index('< ZONE >'))
 if(zone != '< ZONE >'):
     val = df.loc[(df['NAME'] == product) & (df['ZONE'] == zone)]
-    maxmrp = str(max(val['MRP']))
-    minmrp = str(min(val['MRP']))
-    mrpr = 'Previous MRP Range : ' + minmrp + ' - ' + maxmrp
+    pricerange = val['Price_Range']
+    mrpr = 'Previous MRP Range : ' + pricerange
     st.write(mrpr)
 mrp = st.number_input("Enter MRP")
 
@@ -149,11 +159,3 @@ gp = st.button('Get Price')
 if(gp):
     if((category != '< CATEGORY >' ) and (brand != '< BRAND >') and (product != '< PRODUCT >') and (zone != '< ZONE >')):
         getprice()
-# with st.sidebar:
-#     ld = st.button('Load Data')
-
-# if(ld):
-#     with st.spinner('Loading Data...'):
-#         load_data()
-#         df3  = pd.read_csv('C:\\Users\\amalj\\Desktop\\360\\coefs.csv')         
-#     st.success('Done!')
